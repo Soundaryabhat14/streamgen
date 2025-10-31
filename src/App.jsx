6c17b8b48e8a417fd27baf2e4861f50f5d5954d8
soundarya <soundarya@whatsloan.com>
2025-10-31 19:09:28 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className= "font-bold text-3xl italic text-center">
+    Hello Vite + React!
+   </div>
+  )
+}
+
+export default App
